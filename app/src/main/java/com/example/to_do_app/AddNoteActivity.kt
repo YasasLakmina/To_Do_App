03160af2edd8ae94264ca36123eef094e2c7906b
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-05-11 21:16:45 +0530

    commit 3

--- app/src/main/java/com/example/to_do_app/AddNoteActivity.kt
+++ app/src/main/java/com/example/to_do_app/AddNoteActivity.kt
@@ -1,20 +1,30 @@
 package com.example.to_do_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.to_do_app.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_note)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/to_do_app/MainActivity.kt
+++ app/src/main/java/com/example/to_do_app/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.to_do_app
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.to_do_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
